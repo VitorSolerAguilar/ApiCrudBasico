02ed96655adaad0116bd8cd17a6a78b77113b4e6
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-10-03 22:52:30 -0300

    Repositorio de dados

--- Program.cs
+++ Program.cs
@@ -3,6 +3,25 @@ var app = builder.Build();
 
 app.Run();
 
+public static class ProdutoRepository{
+    public static List<Produto> Produtos;
+
+    public static Produto ObterProduto(int id){
+        return Produtos.FirstOrDefault(p => p.Id == id);
+    }
+
+    public static void AdicionarProduto(Produto produto){
+        if(Produtos == null){
+            Produtos = new List<Produto>();
+        }
+        Produtos.Add(produto);
+    }
+
+    public static void RemoverProdutos(Produto produto){
+        Produtos.Remove(produto);
+    }
+}
+
 public class Produto{
     public int Id { get; set; }
     public string Nome { get; set; }
