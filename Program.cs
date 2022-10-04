9a5cdcc9894d8afe0ad2eec915335bac08def141
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-10-03 22:45:44 -0300

    Classe produto implementada

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@ var app = builder.Build();
 
 app.Run();
 
+public class Produto{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public string Descricao { get; set; }
+    public int Quantidade { get; set; }
+}
